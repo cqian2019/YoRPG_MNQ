f0a69bebfd5a032eb0b0dbae7e6cf0dcf7b88426
Cheryl Qian <cqian@stuy.edu>
2017-11-13 22:57:39 -0500

    Added monsters Dog, Dragon, Giant

---
+++ v3/Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Monster{
+
+    public String about() {
+	return "It's just a dog. Should you challenge him?";
+	    }
+    public void setAtt() {;
+	defense += 50;
+	attRating += 0.2;
+	    }
+}
+    
+
