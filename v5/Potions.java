f0d588ca9f2996425ab7224ab9f1689d45195ed2
Cheryl Qian <cqian@stuy.edu>
2017-11-15 21:57:45 -0500

    added 3 potions

---
+++ v5/Potions.java
@@ -0,0 +1,14 @@
+public abstract class Potions {
+
+    public static void defPot() {
+	defense += 20;
+    }
+
+    public static void attPot() {
+	attRating += 0.5;
+    }
+
+    public static void hpPot() {
+	health += 50;
+    }
+}
